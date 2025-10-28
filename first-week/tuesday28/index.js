b974bac3f88aff0201c14606f73df2665ae68e4f
pedrohrdev <pedrohrdev@gmail.com>
2025-10-28 19:27:57 -0300

    Adding the day's file and setting daily goals

---
+++ first-week/tuesday28/index.js
@@ -0,0 +1,11 @@
+/*
+    Daily objective: 
+        1 - Learn Destructuring;
+
+            1.1 - Create examples of destructuring;
+        
+        2 - Learn spred/rest operators;
+
+            2.1 - Make a function that returns any number of arguments (rest);
+
+*/
