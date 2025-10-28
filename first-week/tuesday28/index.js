2326e26f028349eebbcde870f08eb46c26308b47
pedrohrdev <pedrohrdev@gmail.com>
2025-10-28 19:29:23 -0300

    Destructuring in Arrays

--- first-week/tuesday28/index.js
+++ first-week/tuesday28/index.js
@@ -9,3 +9,27 @@
             2.1 - Make a function that returns any number of arguments (rest);
 
 */
+
+// =================================== DESTRUCTURING in Arrays ==============================================
+
+
+
+const nums = [10, 20, 30]
+
+
+
+const [a = 5, b = 5, c = 5] = nums; // <-- It is possible to pass default values ​​if there is no value;
+
+
+
+// console.log(a, b, c) <-This will return 10, 20, 30;
+
+
+
+const [x, , z] = nums; // <-- It is possible to skip a value if you do not want to assign it to a variable.
+
+console.log(x, z)
+
+
+
+// ========================================================================================================== //
