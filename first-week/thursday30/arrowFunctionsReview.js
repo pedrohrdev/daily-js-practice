b722aebb2bc95531b466226694ab2d3f9f44d366
pedrohrdev <pedrohrdev@gmail.com>
2025-10-30 15:55:29 -0300

    Review Day: Arrow Functions

---
+++ first-week/thursday30/arrowFunctionsReview.js
@@ -0,0 +1,10 @@
+// 1 - Arrow Functions vs Traditional Functions, Arrow functions
+// are shorter and inherit 'this' from their parent scope.
+
+// Traditional function example:
+function sum(a, b) {
+  return a + b;
+}
+
+// Same logic with an arrow function:
+const sumArrow = (a, b) => a + b;
